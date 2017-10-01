ca70d73cdafbfa2033c21682971a20660d2d99e0
Karel Zavadil <mojemejly@centrum.cz>
2017-10-01 19:10:11 +0200

    error

--- src/modules/errorlog.php
+++ src/modules/errorlog.php
@@ -2,8 +2,12 @@
 
 class errorlogModule extends zModule {
 
-	public $path = 'error.log';
-		
+	public $path = '/var/log/zengine.log';
+
+	public function onEnabled() {
+		$this->base_url = $this->getConfigValue('error_log_path', $this->path);
+	}
+
 	public function write($message) {
 		$myfile = fopen($this->path, 'a');
 		fwrite($myfile, sprintf('%s: %s', date('Y-m-d H:i:s'), $message . PHP_EOL));
