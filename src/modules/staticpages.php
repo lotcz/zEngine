2c6ca6b8ead1e2e4a0d796b07e96a1fa7c855b70
Karel Zavadil <mojemejly@centrum.cz>
2017-11-05 23:03:50 +0100

    jhtmlarea

--- src/modules/staticpages.php
+++ src/modules/staticpages.php
@@ -5,9 +5,9 @@ require_once __DIR__ . '/../app/models/static_page.m.php';
 class staticpagesModule extends zModule {
 
 	public function activateEditor() {
-		$this->z->core->includeJS('resources/jHtmlArea-0.8.min.js');
+		$this->z->core->includeJS('resources/jhtmlarea.min.js');
 		$this->z->core->includeJS('resources/staticpages.js');
-		$this->z->core->includeCSS('resources/jHtmlArea.css');
+		$this->z->core->includeCSS('resources/jhtmlarea.css');
 	}
 
 }
