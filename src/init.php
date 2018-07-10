21889cec9e568266da34ec3423c620758ae30c20
Karel Zavadil <mojemejly@centrum.cz>
2018-07-10 12:42:19 +0000

    sql

--- src/init.php
+++ src/init.php
@@ -1,6 +1,6 @@
 <?php
-	require_once __DIR__ . 'zengine.php';	
-	$z = new zEngine(__DIR__ . 'app/');	
+	require_once __DIR__ . '/zengine.php';	
+	$z = new zEngine(__DIR__ . '/app/');	
 	$z->enableModule('mysql');
 	$sql = file_get_contents(__DIR__ . '/../sql/zEngine.sql');
 	zQuery::executeSQL($z->mysql->connection, $sql);
