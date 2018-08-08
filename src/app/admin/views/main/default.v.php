c228893c587113c0c238b05d1f8b5a63a4c87881
Karel Zavadil <mojemejly@centrum.cz>
2018-08-08 17:04:04 +0000

    added class to main-titla

--- src/app/admin/views/main/default.v.php
+++ src/app/admin/views/main/default.v.php
@@ -1,5 +1,5 @@
 <div class="container-fluid p-3">
-	<h1><?=$this->getData('page_title') ?></h1>
+	<h1 class="main-title"><?=$this->getData('page_title') ?></h1>
 
 	<?php
 		$this->renderMessages();
--- src/app/views/main/default.v.php
+++ src/app/views/main/default.v.php
@@ -1,7 +1,6 @@
 <div class="container p-3">
-	<h1><?=$this->getData('page_title') ?></h1>
-	
-	<?php	
+	<h1 class="main-title"><?=$this->getData('page_title') ?></h1>
+	<?php
 		$this->renderMessages();
 		$this->renderPageView();
 	?>
