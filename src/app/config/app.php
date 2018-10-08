e417bf68fadf0732b9706d80b2f1a91e77034ab5
Karel Zavadil <mojemejly@centrum.cz>
2018-10-08 15:13:42 +0000

    4.0

--- src/app/config/app.php
+++ src/app/config/app.php
@@ -7,10 +7,10 @@
 		'version' => 1.0,
 
     // required zEngine major version (integer value)
-		'require_z_version' => 3,
+		'require_z_version' => 4,
 
 		// this is minimum required zEngine version
-		'minimum_z_version' => 3.4,
+		'minimum_z_version' => 4.0,
 
 		// modules that are enabled by default
 		'modules' => ['resources', 'db', 'i18n', 'auth', 'admin'],
