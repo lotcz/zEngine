b60c61d11d5d3ad0f7b5c5b26ed551279eb41a57
Karel Zavadil <zavadil@incomaker.com>
2025-09-20 00:20:41 +0200

    QR as png

--- src/app/modules/qr.php
+++ src/app/modules/qr.php
@@ -17,17 +17,19 @@ class qrModule extends zModule {
 		int $amount_czk,
 		string $recipient_name,
 		bool $is_instant,
-		string|int|null $variable_symbol
+		string|int|null $variable_symbol,
+		int $border = 0,
+		string $file_ext = 'svg'
 	): string {
 		$instant = $is_instant ? "1" : "0";
 		$amount = (string)$amount_czk;
 		$vs = $variable_symbol ? str_pad((string)$variable_symbol, 10, "0", STR_PAD_LEFT) : "";
-		$raw = $this->api_key . $target_account_iban . $amount . $message. $recipient_name . $instant . $vs;
+		$raw = $this->api_key . $target_account_iban . $amount . $message. $recipient_name . $instant . $vs . $border . $file_ext;
 		$hash = md5($raw);
 		$message = urlencode($message);
 		$target_account_iban = urlencode($target_account_iban);
 		$recipient_name = urlencode($recipient_name);
-		$query = "?secure_token=$hash&target_account=$target_account_iban&message=$message&amount=$amount&recipient_name=$recipient_name&is_instant=$instant&variable_symbol=$vs";
+		$query = "?secure_token=$hash&target_account=$target_account_iban&message=$message&amount=$amount&recipient_name=$recipient_name&is_instant=$instant&variable_symbol=$vs&border=$border&file_ext=$file_ext";
 		return z::getUrl($this->base_url, "bank-transfer-secured") . $query;
 	}
 
